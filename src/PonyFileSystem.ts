e32c866fa3cc806b17fd79f104f5ec15596418c6
thingalon <mark@thingalon.com>
2019-03-24 14:04:26 +1100

    Code cleanup
    Clean up leftover vestigial bits from the MemFS example codebase.

--- src/PonyFileSystem.ts
+++ src/PonyFileSystem.ts
@@ -3,51 +3,15 @@ import * as vscode from 'vscode';
 import { Host, HostConfig } from './Host';
 import { hostname } from 'os';
 
-export class File implements vscode.FileStat {
-
-    type: vscode.FileType;
-    mtime: number;
-    ctime: number;
-    size: number;
-
-    name: string;
-    data?: Uint8Array;
-
-    constructor(name: string) {
-        this.type = vscode.FileType.File;
-        this.ctime = Date.now();
-        this.mtime = Date.now();
-        this.size = 0;
-        this.name = name;
-    }
-}
-
-export class Directory implements vscode.FileStat {
-
-    type: vscode.FileType;
-    ctime: number;
-    mtime: number;
-    size: number;
-
-    name: string;
-    entries: Map<string, File | Directory>;
-
-    constructor(name: string) {
-        this.type = vscode.FileType.Directory;
-        this.ctime = Date.now();
-        this.mtime = Date.now();
-        this.size = 0;
-        this.name = name;
-        this.entries = new Map();
-    }
-}
-
-export type Entry = File | Directory;
-
 export class PonyFileSystem implements vscode.FileSystemProvider {
 
     private availableHosts: { [name: string]: HostConfig };
     private activeHosts: { [name: string]: Host };
+
+    private emitter = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
+    private bufferedEvents: vscode.FileChangeEvent[] = [];
+    private fireSoonHandle?: NodeJS.Timer;
+    readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this.emitter.event;
 
     constructor( context: vscode.ExtensionContext ) {
         this.activeHosts = {};
@@ -77,7 +41,7 @@ export class PonyFileSystem implements vscode.FileSystemProvider {
         const [ host, remotePath ] = this.splitPath( uri.path );
         await host.writeFile( 0, remotePath, content, options );
 
-        this._fireSoon({ type: vscode.FileChangeType.Changed, uri });
+        this.fireSoon({ type: vscode.FileChangeType.Changed, uri });
     }
 
     public async rename( oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean } ) {
@@ -94,7 +58,7 @@ export class PonyFileSystem implements vscode.FileSystemProvider {
 
         await newHost.rename( 0, oldPath, newPath, options );
 
-        this._fireSoon(
+        this.fireSoon(
             { type: vscode.FileChangeType.Deleted, uri: oldUri },
             { type: vscode.FileChangeType.Created, uri: newUri }
         );
@@ -104,14 +68,19 @@ export class PonyFileSystem implements vscode.FileSystemProvider {
         const [ host, remotePath ] = this.splitPath( uri.path );
         await host.delete( 0, remotePath );
 
-        this._fireSoon( { type: vscode.FileChangeType.Deleted, uri } );
+        this.fireSoon( { type: vscode.FileChangeType.Deleted, uri } );
     }
 
     async createDirectory( uri: vscode.Uri ) {
         const [ host, remotePath ] = this.splitPath( uri.path );
         await host.mkdir( 0, remotePath );
 
-        this._fireSoon( { type: vscode.FileChangeType.Created, uri } );
+        this.fireSoon( { type: vscode.FileChangeType.Created, uri } );
+    }
+
+    public watch( resource: vscode.Uri, opts: any ): vscode.Disposable {
+        // TODO.
+        return new vscode.Disposable( () => {} );
     }
 
     private splitPath( fullPath: string ): [ Host, string ] {
@@ -149,78 +118,12 @@ export class PonyFileSystem implements vscode.FileSystemProvider {
         return hosts;
     }
 
-    // --- manage file contents
-
-    root = new Directory('');
-
-
-    // --- lookup
-
-    private _lookup(uri: vscode.Uri, silent: false): Entry;
-    private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined;
-    private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined {
-        let parts = uri.path.split('/');
-        let entry: Entry = this.root;
-        for (const part of parts) {
-            if (!part) {
-                continue;
-            }
-            let child: Entry | undefined;
-            if (entry instanceof Directory) {
-                child = entry.entries.get(part);
-            }
-            if (!child) {
-                if (!silent) {
-                    throw vscode.FileSystemError.FileNotFound(uri);
-                } else {
-                    return undefined;
-                }
-            }
-            entry = child;
-        }
-        return entry;
-    }
-
-    private _lookupAsDirectory(uri: vscode.Uri, silent: boolean): Directory {
-        let entry = this._lookup(uri, silent);
-        if (entry instanceof Directory) {
-            return entry;
-        }
-        throw vscode.FileSystemError.FileNotADirectory(uri);
-    }
-
-    private _lookupAsFile(uri: vscode.Uri, silent: boolean): File {
-        let entry = this._lookup(uri, silent);
-        if (entry instanceof File) {
-            return entry;
-        }
-        throw vscode.FileSystemError.FileIsADirectory(uri);
-    }
-
-    private _lookupParentDirectory(uri: vscode.Uri): Directory {
-        const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-        return this._lookupAsDirectory(dirname, false);
-    }
-
-    // --- manage file events
-
-    private _emitter = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
-    private _bufferedEvents: vscode.FileChangeEvent[] = [];
-    private _fireSoonHandle?: NodeJS.Timer;
-
-    readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this._emitter.event;
-
-    watch(resource: vscode.Uri, opts: any): vscode.Disposable {
-        // ignore, fires for all changes...
-        return new vscode.Disposable(() => { });
-    }
-
-    private _fireSoon(...events: vscode.FileChangeEvent[]): void {
-        this._bufferedEvents.push(...events);
-        clearTimeout(this._fireSoonHandle!);
-        this._fireSoonHandle = setTimeout(() => {
-            this._emitter.fire(this._bufferedEvents);
-            this._bufferedEvents.length = 0;
+    private fireSoon( ...events: vscode.FileChangeEvent[] ): void {
+        this.bufferedEvents.push( ...events );
+        clearTimeout( this.fireSoonHandle! );
+        this.fireSoonHandle = setTimeout( () => {
+            this.emitter.fire( this.bufferedEvents );
+            this.bufferedEvents.length = 0;
         }, 5);
     }
 }
